fc6f222c1602a9f3f2533082191a5cddcae11a7c
andukdahacker <82881040+andukdahacker@users.noreply.github.com>
2025-11-02 02:39:05 +0700

    update

--- apps/backend/src/generated/prisma/client/client.ts
+++ apps/backend/src/generated/prisma/client/client.ts
@@ -41,6 +41,10 @@ export { Prisma }
 path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node")
 path.join(process.cwd(), "src/generated/prisma/client/libquery_engine-darwin-arm64.dylib.node")
 
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-linux-musl-openssl-3.0.x.so.node")
+path.join(process.cwd(), "src/generated/prisma/client/libquery_engine-linux-musl-openssl-3.0.x.so.node")
+
 /**
  * Model Center
  * 
